68ad8fd580184adc6fc3cad4575db1e11f2f82ac
Ildar <mexx0174@gmail.com>
2023-02-26 15:48:00 +0500

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание 1");
+        List<Integer> integerList = List.of(100, 211, -101, 444, 555, 12, 1232, 441, 881, 10,51);
+        findMinMax(integerList.stream(),
+                Integer::compareTo,
+                (x, y) -> System.out.println("Min number: " + x + "\nMax number: " + y));
+
+        System.out.println("\nЗадание 2");
+        evenNumbers(integerList);
+    }
+
+
+    private static <T> void findMinMax(Stream<? extends T> stream,
+                                       Comparator<? super T> order,
+                                       BiConsumer<? super T, ? super T> minMaxConsumer) {
+
+        List<? extends T> list = stream.sorted(order).toList();
+
+        if (!list.isEmpty()) {
+            minMaxConsumer.accept(list.get(0), list.get(list.size() - 1));
+        } else {
+            minMaxConsumer.accept(null, null);
+        }
+
+    }
+
+    private static void evenNumbers(List<Integer> integers) {
+        System.out.println("Количество четных чисел: " + integers.stream()
+                .filter(x -> x % 2 == 0)
+                .peek(System.out::println)
+                .count());
     }
 }
